c3647b09304da0a8639953192206c35d261ddc14
Paulpoomrit <paulpoomrit@gmail.com>
2025-06-07 20:29:44 -0700

    Configure FBX loader to load the duck

--- src/components/web-gl-component/web-gl-component.tsx
+++ src/components/web-gl-component/web-gl-component.tsx
@@ -27,10 +27,17 @@ export const WebGLComponent = ({ className }: WebGLComponentProps) => {
         const renderer = new THREE.WebGLRenderer( {antialias: true} );
         renderer.setSize(container.clientWidth, container.clientHeight);
 
+
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.enableDamping = true;
         // controls.target.set(0, 1, 0);
 
+        const environmentTexture = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+        scene.environment = environmentTexture
+        scene.background = environmentTexture
+
+        const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+        scene.add(ambientLight)
 
         container.appendChild(renderer.domElement);
 
@@ -44,18 +51,42 @@ export const WebGLComponent = ({ className }: WebGLComponentProps) => {
         window.addEventListener('resize', handleResize);
 
         const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshNormalMaterial({ wireframe: true });
+        const material = new THREE.MeshNormalMaterial({ wireframe: false });
 
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+        // const cube = new THREE.Mesh(geometry, material);
+        // scene.add(cube);
+
+        const fbxLoader = new FBXLoader();
+        fbxLoader.load(
+            'Duck.FBX',
+            (object) => {
+                object.traverse(function (child) {
+                    if ((child as THREE.Mesh).isMesh) {
+                        (child as THREE.Mesh).material = material
+                        if ((child as THREE.Mesh).material) {
+                            ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+                        }
+                    }
+                })
+                object.scale.set(.02, .02, .02)
+                object.position.set(0,-0.5,0);
+                scene.add(object)
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
 
         let animationId: number;
         function animate() {
             animationId =  requestAnimationFrame(animate);
             controls.update();
 
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            // cube.rotation.x += 0.01;
+            // cube.rotation.y += 0.01;
 
             renderer.render(scene, camera);
         }
